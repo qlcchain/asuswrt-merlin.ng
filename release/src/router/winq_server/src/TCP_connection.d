31028e74a2ac2cfb7fc6f24ab9c176a40194e9c5
lichao <chao.li@qlink.mobi>
2018-12-19 03:17:18 +0000

    delete compile files

--- release/src/router/winq_server/src/TCP_connection.d
+++
@@ -1,5 +0,0 @@
-TCP_connection.o: src/TCP_connection.c inc/TCP_connection.h \
- inc/TCP_client.h inc/TCP_server.h inc/crypto_core.h inc/list.h \
- inc/onion.h inc/DHT.h inc/logger.h inc/network.h inc/ccompat.h \
- inc/ping_array.h inc/util.h
-	arm-brcm-linux-uclibcgnueabi-gcc -DRTAC68U -DRTAC68U -DRTAC68U -DBCMWPA2 -DBCMQOS -DEXT_ACS -DD11AC_IOTYPES -DPHYMON -DPROXYARP -DTRAFFIC_MGMT -DTRAFFIC_MGMT_RSSI_POLICY -DMFP -D__CONFIG_MFP__ -DLINUX26 -DCONFIG_BCMWL5 -DDEBUG_NOISY -DDEBUG_RCTEST -pipe -DBCMWPA2 -DBCMARM -fno-strict-aliasing -marm -DRTCONFIG_NVRAM_64K -DLINUX_KERNEL_VERSION=132644  -DRTAC68U -O2 -DBCM_DCS -D__CONFIG_EMF__ -DNAS_GTK_PER_STA -D__CONFIG_WPS__ -Wall -g  -I./inc  -I /home/lichao/asuswrt-merlin.ng/release/src-rt-6.x.4708/router/qrencode-4.0.0/ -I /home/lichao/asuswrt-merlin.ng/release/src-rt-6.x.4708/router/libsodium-1.0.16/src/libsodium/include/ -I /home/lichao/asuswrt-merlin.ng/release/src-rt-6.x.4708/router/curl-7.21.7/include/ -I /home/lichao/asuswrt-merlin.ng/release/src-rt-6.x.4708/router/libpng-1.6.34/ -c src/TCP_connection.c -o  src/TCP_connection.o

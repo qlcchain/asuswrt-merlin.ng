c76680dba9d8bf6b44449a22d961a3eba376bac5
lichao <chao.li@qlink.mobi>
2018-12-19 03:10:27 +0000

    update ip renew script

--- release/src/router/winq_server/files/getrealipwinq.sh
+++ release/src/router/winq_server/files/getrealipwinq.sh
@@ -37,7 +37,7 @@ if [ "$(nvram get wans_mode)" = "lb" ] ; then
 		nvram set ${prefix}realip_ip=$result
 
 		wan=`echo $prefix|sed -e "s,_,,"`
-		[ -z "$result" ] && echo "$wan failed." || echo "$result"
+		[ -z "$result" ] && echo "" || echo "$result"
 	done
 else
 	for prefix in $prefixes; do
@@ -65,5 +65,5 @@ else
 	nvram set ${prefix}realip_state=$state
 	nvram set ${prefix}realip_ip=$result
 
-	[ -z "$result" ] && echo "Failed." || echo "$result"
+	[ -z "$result" ] && echo "" || echo "$result"
 fi
